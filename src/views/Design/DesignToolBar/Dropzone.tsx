978b4a8f102f72875e06c96d691c474c0b646511
chenlizan <clz_318@live.com>
2019-01-18 17:40:30 +0800

    backup 1.18

--- src/views/Design/DesignToolBar/Dropzone.tsx
+++ src/views/Design/DesignToolBar/Dropzone.tsx
@@ -2,6 +2,10 @@ import * as React from 'react'
 import * as PropTypes from 'prop-types';
 import DropZone from 'react-dropzone'
 
+declare var Babel: any;
+// @ts-ignore
+// import Babel from 'babel-standalone';
+
 const parseJson = require('parse-json');
 const stripBom = require('strip-bom');
 
@@ -27,7 +31,19 @@ export default class Dropzone extends React.Component<DropzoneProps, any> {
         const type = acceptedFiles[0].type;
         reader.onloadend = (e) => {
             if (type === "text/javascript") {
-                (this.props as any).onChange(eval(stripBom((e.target as any).result)));
+                const babelConfig = {
+                    babelrc:false,
+                    presets: [
+                        'es2015',
+                        'react',
+                    ],
+                    sourceType:"module"
+                };
+                const output = Babel.transform(stripBom((e.target as any).result), babelConfig);
+                const node1 = React.createElement(eval(output.code));
+                console.log(output.code);
+                React.isValidElement(node1);
+                // (this.props as any).onChange(eval(stripBom((e.target as any).result)));
             } else if (type === "application/json") {
                 (this.props as any).onChange(parseJson((stripBom((e.target as any).result))));
             }
