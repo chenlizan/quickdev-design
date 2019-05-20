7a97c9890e35c6587ddeaf5a1fea339155e50195
chenlizan <clz_318@live.com>
2019-05-20 15:38:04 +0800

    5.20

--- src/assets/json/AttributeConfig/index.ts
+++ src/assets/json/AttributeConfig/index.ts
@@ -1,6 +1,7 @@
 export default {
     "antd-mobile": {
         "Button": require('./antd-mobile/Button.json'),
-        "Flex": require('./antd-mobile/Flex.json')
+        "Flex": require('./antd-mobile/Flex.json'),
+        "Flex.Item": require('./antd-mobile/FlexItem.json')
     }
 }
